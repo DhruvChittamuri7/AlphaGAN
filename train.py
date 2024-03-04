bab9a97a49f6c24b76188d3b5b6c2cae1cc7d33b
Krish <jkrish@cs.washington.edu>
2024-03-04 15:25:25 -0800

    Train

---
+++ train.py
@@ -0,0 +1,33 @@
+import os
+from PIL import Image
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+import Encoder
+import Decoder
+import Discriminator
+
+class AlphaMattingDataset(Dataset):
+    def __init__(self, input_dir, gt_dir):
+        self.input_dir = input_dir
+        self.gt_dir = gt_dir
+        self.images = os.listdir(self.input_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.input_dir, self.images[idx])
+        gt_name = os.path.join(self.gt_dir, self.images[idx].replace('input', 'gt'))  
+        
+        image = Image.open(img_name).convert("RGB")
+        gt_image = Image.open(gt_name).convert("L") 
+
+        return image, gt_image
+
+
+train_dataset = AlphaMattingDataset(input_dir='Data/Train/InputImages', gt_dir='Data/Train/GroundTruthAlphas')
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=2)
+
+encoder = Encoder.Encoder()
+decoder = Decoder.Decoder()
+discriminator = Discriminator.PatchGANDiscriminator(input_channels=3) 
