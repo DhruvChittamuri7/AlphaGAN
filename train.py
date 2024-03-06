52697f4098e563d02a879bc4e20fff546be0c59f
2265464 <63368654+2265464@users.noreply.github.com>
2024-03-06 00:30:05 -0800

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,12 @@
 import os
+import numpy as np
 from PIL import Image
+import torch as t
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
-import Encoder
-import Decoder
+import Generator
 import Discriminator
+
 
 class AlphaMattingDataset(Dataset):
     def __init__(self, input_dir, gt_dir, trimaps):
@@ -17,18 +19,32 @@ class AlphaMattingDataset(Dataset):
         return len(self.images)
 
     def __getitem__(self, idx):
-        img_name = os.path.join(self.input_dir, "GT" + self.images[idx] + ".png")
-        gt_name = os.path.join(self.gt_dir, "GT" + self.images[idx] + ".png") 
-        
+        img_name = os.path.join(self.input_dir, self.images[idx])
+        tri_name = os.path.join(self.trimaps, "Trimap1/" + self.images[idx])
+        gt_name = os.path.join(self.gt_dir, self.images[idx])
+
         image = Image.open(img_name).convert("RGB")
-        gt_image = Image.open(gt_name).convert("L") 
+        tri_image = Image.open(tri_name).convert("L")
+        gt_image = Image.open(gt_name).convert("L")
 
-        return image, gt_image
+        image = transformer(image)
+        tri_image = transformer(tri_image)
+        gt_image = transformer(gt_image)
+
+        return image, tri_image, gt_image
 
 
-train_dataset = AlphaMattingDataset(input_dir='Data/Train/InputImages', gt_dir='Data/Train/GroundTruthAlphas', trimaps = 'Data/Train/Trimaps')
+transformer = transforms.Compose([transforms.ToTensor()])
+
+train_dataset = AlphaMattingDataset(input_dir='Data/Train/InputImages', gt_dir='Data/Train/GroundTruthAlphas',
+                                    trimaps='Data/Train/Trimaps')
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=2)
 
-encoder = Encoder.Encoder()
-decoder = Decoder.Decoder()
-discriminator = Discriminator.PatchGANDiscriminator(input_channels=3) 
+generator = Generator.Generator()
+discriminator = Discriminator.PatchGANDiscriminator(input_channels=3)
+
+device = t.device("cuda" if t.cuda.is_available() else "cpu")
+
+for image, tri_image, gt_image in train_dataset:
+    input_img = t.tensor(np.concatenate((image.numpy(), tri_image.numpy()), axis=0)).to(device)
+    gt_img = gt_image.to(device)
